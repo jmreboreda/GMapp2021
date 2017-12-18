c2b3bed50a3945b53e14f4e4e29898ea657ef544
jmreboreda <jmreboreda@mundo-r.com>
2017-12-18 10:44:41 +0100

    Commit from windows

--- src/main/java/gmoldes/App.java
+++ src/main/java/gmoldes/App.java
@@ -20,7 +20,7 @@ public class App extends Application
     public void start(Stage primaryStage) throws IOException {
         MainController controller = new MainController();
         primaryStage.setResizable(false);
-        Scene scene = new Scene(controller,720,650);
+        Scene scene = new Scene(controller,730,650);
         //scene.getStylesheets().add(App.class.getResource("lcd.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
